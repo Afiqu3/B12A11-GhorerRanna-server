6337dd48f3383bb7c341550c97a2e8f59b4fbc0d
Afiqu3 <gahossainj@gmail.com>
2025-12-08 22:07:22 +0600

    set up jwt token

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,8 +10,29 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+const verifyJWTToken = (req, res, next) => {
+  // console.log(req.headers);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+  const token = authorization.split(' ')[1];
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    // console.log(decoded)
+    req.token_email = decoded.email;
+    next();
+  });
+};
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
 });
 
 const uri = process.env.DB_HOST;
@@ -20,16 +42,31 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const ghorerRannaDB = client.db('ghorerRannaDB');
+    const usersCollection = ghorerRannaDB.collection('users');
+
+    app.post('/getToken', async (req, res) => {
+      const loggedUser = req.body;
+      const token = jwt.sign(loggedUser, process.env.JWT_SECRET, {
+        expiresIn: '12h',
+      });
+      res.send({ token: token });
+    });
+
+    // app.get('/hello', verifyJWTToken, async (req, res) => {
+    //   res.send({ message: 'Hello from the other side' });
+    // });
+
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
